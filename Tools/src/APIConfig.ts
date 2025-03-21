d5d154f49ce7f9fead61fa5752b86ed0843ed851
Lucas Chan <105584676+kaihong118@users.noreply.github.com>
2025-03-21 15:59:32 +0800

    Delete Tools directory

--- Tools/src/APIConfig.ts
+++
@@ -1,9 +0,0 @@
-import { AxiosConfig } from '@wallet-manager/node-package-axios';
-
-export default interface APIConfig {
-  name: string;
-  axios: {
-    swapAgentAccessServer: AxiosConfig;
-    abccWalletAccessServer: AxiosConfig;
-  };
-}
