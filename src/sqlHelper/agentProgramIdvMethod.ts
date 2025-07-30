df098d00bfbfc64596acbed5562add9d23463c57
Lucas Chan <eu39@18m.dev>
2025-07-30 16:17:02 +0800

    no message

---
+++ src/sqlHelper/agentProgramIdvMethod.ts
@@ -0,0 +1,87 @@
+import fs from 'fs';
+import readline from 'readline';
+
+const template = `
+-- :programName
+UPDATE swap_agent.agent_programs
+SET
+  kyc_idv_method = :kycMethod,
+  question_type = :questionType,
+  last_modified_date = now ()
+WHERE
+  id = ':id'
+  AND program_agent_id = ':programAgentId'
+  AND program_name = ':programName';
+`;
+
+const data = [
+  {
+    id: 163,
+    program_agent_id: 'PA-G001',
+    program_name: 'VQuick-G001-EXT-P11',
+    kyc_idv_method: 3,
+    question_type: 2,
+  },
+  {
+    id: 164,
+    program_agent_id: 'PA-G002',
+    program_name: 'VQuick-G002-EXT-P12',
+    kyc_idv_method: 3,
+    question_type: 2,
+  },
+];
+
+function generateSQL(
+  template: string,
+  params: Record<string, string | number>
+): string {
+  return template.replace(/:([a-zA-Z0-9_]+)/g, (_, key) => {
+    const val = params[key];
+    if (val === undefined) {
+      throw new Error(`Missing value for placeholder ":${key}"`);
+    }
+    return String(val);
+  });
+}
+
+function askQuestion(question: string): Promise<string> {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  return new Promise((resolve) => {
+    rl.question(question, (answer) => {
+      rl.close();
+      resolve(answer.trim());
+    });
+  });
+}
+
+async function run() {
+  const ticketNo = await askQuestion('Enter the ticket number: ');
+
+  let outputSql = 'BEGIN;\n';
+
+  for (const x of data) {
+    const sql = generateSQL(template, {
+      id: x.id,
+      programAgentId: x.program_agent_id,
+      programName: x.program_name,
+      kycMethod: 1,
+      questionType: 2,
+    });
+
+    outputSql += sql;
+  }
+  outputSql += '\nCOMMIT;';
+
+  fs.writeFileSync(
+    `/Users/lucas/Downloads/SQL/[swap_agent] Data patch to update idvMethod and questionType (${ticketNo}).sql`,
+    outputSql,
+    'utf8'
+  );
+  console.log(`✅ SQL file written`);
+}
+
+run();
---
+++ src/sqlHelper/eventMapping.ts
@@ -0,0 +1,87 @@
+import fs from 'fs';
+import readline from 'readline';
+
+const template = `
+--:eventType
+INSERT INTO
+    pmp_access.event_message_mappings (
+        merchant_id,
+        topic,
+        event_type,
+        message_type,
+        send_tg_message
+    )
+VALUES
+    (
+        :merchantId,
+        ':topic',
+        ':eventType',
+        :messageType,
+        :sendTgMessage
+    );
+`;
+
+const data = [
+  {
+    merchant_id: 6,
+    topic: 'EVENT_OUT_PMP_PROGRAM',
+    event_type: 'online-transaction',
+    message_type: 1,
+    send_tg_message: false,
+  },
+];
+
+function generateSQL(
+  template: string,
+  params: Record<string, string | number | boolean>
+): string {
+  return template.replace(/:([a-zA-Z0-9_]+)/g, (_, key) => {
+    const val = params[key];
+    if (val === undefined) {
+      throw new Error(`Missing value for placeholder ":${key}"`);
+    }
+    return String(val);
+  });
+}
+
+function askQuestion(question: string): Promise<string> {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  return new Promise((resolve) => {
+    rl.question(question, (answer) => {
+      rl.close();
+      resolve(answer.trim());
+    });
+  });
+}
+
+async function run() {
+  // const ticketNo = await askQuestion('Enter the ticket number: ');
+
+  let outputSql = 'BEGIN;\n';
+
+  for (const x of data) {
+    const sql = generateSQL(template, {
+      merchantId: x.merchant_id,
+      topic: x.topic,
+      eventType: x.event_type,
+      messageType: x.message_type,
+      sendTgMessage: x.send_tg_message,
+    });
+
+    outputSql += sql;
+  }
+  outputSql += '\nCOMMIT;';
+
+  fs.writeFileSync(
+    `/Users/lucas/Downloads/SQL/[pmp_access] Add new event mapping.sql`,
+    outputSql,
+    'utf8'
+  );
+  console.log(`✅ SQL file written`);
+}
+
+run();
