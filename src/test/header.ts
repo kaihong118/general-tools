39327b664b90367e6a4cc9775e29bc7004850da2
Lucas Chan <eu39@18m.dev>
2025-07-30 12:01:17 +0800

    no message

--- src/test/header.ts
+++ src/test/header.ts
@@ -3,63 +3,84 @@
  */
 
 import { MessageHelper } from '@wallet-manager/node-package-util';
-import { ethers } from 'ethers';
+import moment from 'moment';
 
-const body = {
-  totalAmount: 544,
-  currency: 'HKDM',
-  customerId: 'd317a868-928f-4c4f-9d0b-985f22c4f0ed',
-  orderId: 'OR-99',
-  payments: [
-    {
-      paymentType: 1,
-      customerNumber: '3002X10001627100478',
-      currency: 'HKDM-BEP20',
-      amount: 544000000,
-      decimals: 6,
-    },
-  ],
+const data = {
+  merchantId: 6,
+  programAgentId: 'PA-ABCC',
+  distributorAgentId: 'DA-ABCC@PA-ABCC',
+  firstName: 'TESTING',
+  lastName: 'ABCC',
+  email: 'eu39+abcc14@18m.dev',
+  phoneCountryCode: '+852',
+  phoneNumber: '98765432',
+  customerNumber: '3002X10002560940136',
+  idIssuedBy: '*N/A*',
+  idType: 3,
+  idNumber: '123112',
+  dateOfBirth: '1992-11-08',
+  referralCode: 'ABCC_REFERRAL_CODE',
+  questionVersion: 2,
+  answers: {
+    title: 1,
+    alias: '***N/A***',
+    chineseName: '***N/A***',
+    gender: 'Male',
+    idTypeOthers: null,
+    idDateOfIssue: '0000-00-00',
+    idDateOfExpiry: '0000-00-00',
+    nationality: 'KOR',
+    usCitizenship: false,
+    purposeForApplying: 9999,
+    purposeForApplyingOthers: '***N/A***',
+    maritalStatus: 1,
+    educationLevel: 9999,
+    educationLevelOthers: '***N/A***',
+    authorizedToThirdParty: false,
+    companyName: '***N/A***',
+    jobTitle: 9999,
+    jobTitleOthers: '***N/A***',
+    industry: 9999,
+    industryOthers: '***N/A***',
+    employmentStatus: 1,
+    employmentStartDate: null,
+    monthlySalaryHKD: '***N/A***',
+    otherIncomeHKD: '***N/A***',
+    officeTelephoneNumber: '***N/A***',
+    residentialStatus: 9999,
+    residentialStatusOthers: '***N/A***',
+    residentialTelephoneNumber: '***N/A***',
+    residentialAddressLine1: '45 Jongno 3-ga, Jongno-gu, Seoul, South Korea ',
+    residentialAddressLine2: null,
+    residentialAddressLine3: null,
+    residentialPostalCode: '03198',
+    residentialCity: 'Seoul',
+    residentialCountry: 'KOR',
+    deliveryAddressLine1: 'Verve Residences Tower 2, Unit 2017 ',
+    deliveryAddressLine2: '9th Avenue corner 26th Street',
+    deliveryAddressLine3: 'Bonifacio Global City, Taguig City',
+    deliveryPostalCode: '1634',
+    deliveryCity: 'Metro Manila',
+    deliveryCountry: 'PHL',
+  },
 };
-
-const stringJSON = `${JSON.stringify(body)}`;
-const message = `1729497338000#7225665#9#${stringJSON}`;
-const providedSignature =
-  '0xbcd68872d9dd56bc708100b58c8f381da3cc30db3eb20553deb9f77f817732ba17cc45194b49129e8518225a0c2ae0eacc48bbd99b857e5a29f89e6d617809581b';
+const privateKey =
+  '0x8d1f2396e33b56301ea9bb53e0373cf42d899527731a70af68ce68bbf1ab7c77';
+const address = '0x29D02F89EbB2493F24Fcad10E2B329B1CD5e8e0a';
+const session = '0';
+const sequence = 0;
+const timestamp = moment().toISOString();
 
 const test = async () => {
-  const temp = `1729497338000#7225665#9#{"totalAmount":544,"currency":"HKDM","customerId":"d317a868-928f-4c4f-9d0b-985f22c4f0ed","orderId":"OR-99","payments":[{"paymentType":1,"customerNumber":"3002X10001627100478","currency":"HKDM-BEP20","amount":544000000,"decimals":6}]}`;
-  // const temp = message;
-  const _hashedMsg = ethers.utils.toUtf8Bytes(temp);
-  const hashedMessage = ethers.utils.sha256(_hashedMsg);
-
-  // const signature = MessageHelper.sign(
-  //   hashedMessage,
-  //   '0xffc6d50194738bff4bdbd116fab080cde7548cbc47b26577fe65f3ff0c9cfe1c'
-  // );
-
-  const res = MessageHelper.verify(
-    hashedMessage,
-    providedSignature,
-    '0x0c307Bd4Feb71c7f50aD81D3fbB41461B9937b21'
+  const headers = await MessageHelper.createRequestHeader(
+    privateKey,
+    address,
+    session,
+    sequence,
+    data
   );
 
-  console.log(`\n`);
-  console.log(`StringJSON: ${stringJSON}`);
-
-  console.log(`\n`);
-  console.log(`HashedMessage: ${hashedMessage}`);
-
-  console.log(`\n`);
-  console.log(`Provided Signature: ${providedSignature}`);
-
-  // console.log(`\n`);
-  // console.log(`Signature: ${signature}`);
-
-  console.log(`\n`);
-  console.log(`Message: ${message}`);
-
-  console.log(`\n`);
-  console.log(`Result: ${res}`);
+  console.log(headers);
 };
 
 test();
